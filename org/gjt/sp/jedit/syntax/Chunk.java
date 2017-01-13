197418143c0787f96ffe1d32b3840969f41331c3
Rafal Kolanski <rafal.kolanski@data61.csiro.au>
2017-01-13 14:24:32 +1100

    manually apply "brackets_extended_styles" Isabelle 2016-1 patch
    (partially applied already)

--- org/gjt/sp/jedit/syntax/Chunk.java
+++ org/gjt/sp/jedit/syntax/Chunk.java
@@ -259,9 +259,9 @@ public class Chunk extends Token
 	//{{{ Package private members
 
 	//{{{ Instance variables
-	SyntaxStyle style;
+	public SyntaxStyle style;
 	// set up after init()
-	float width;
+	public float width;
 	//}}}
 
 	//{{{ Chunk constructor
@@ -509,7 +509,7 @@ public class Chunk extends Token
 	// this is either style.getBackgroundColor() or
 	// styles[defaultID].getBackgroundColor()
 	private Color background;
-	private String str;
+	public String str;
 	private GlyphVector[] glyphs;
 	//}}}
 
