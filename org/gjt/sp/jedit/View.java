23f1be3a6cad4a2618129c2850f6274104b94604
Rafal Kolanski <rafal.kolanski@nicta.com.au>
2023-12-14 15:28:33 +1100

    don't pre-fill quick search bar

--- org/gjt/sp/jedit/View.java
+++ org/gjt/sp/jedit/View.java
@@ -466,6 +466,8 @@ public class View extends JFrame implements InputHandlerProvider
 				text = SearchAndReplace.escapeRegexp(text,false);
 
 			searchBar.getField().setText(text);
+		} else {
+			searchBar.getField().setText(null);
 		}
 
 		searchBar.getField().requestFocus();
