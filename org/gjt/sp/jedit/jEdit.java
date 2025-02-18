1ca04b83987c8b7bfce469c5be112d5c5fb5f575
Rafal Kolanski <rafal.kolanski@nicta.com.au>
2025-02-18 16:01:52 +1100

    apply Isabelle2025 patch "vfs_marker"+"vfs_manager"

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -4233,7 +4233,7 @@ loop:
 	} //}}}
 
 	//{{{ gotoMarker() method
-	private static void gotoMarker(final View view, final Buffer buffer,
+	public static void gotoMarker(final View view, final Buffer buffer,
 		final String marker)
 	{
 		AwtRunnableQueue.INSTANCE.runAfterIoTasks(new Runnable()
